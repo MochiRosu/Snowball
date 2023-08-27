667671c0760d915362f6a21f35efb7efa17088be
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-08-27 14:28:09 +1000

    pressure plate + door sfx

--- Snowball Fight/Assets/Scripts/PressurePlate.cs
+++ Snowball Fight/Assets/Scripts/PressurePlate.cs
@@ -7,41 +7,89 @@ public class PressurePlate : MonoBehaviour
     public GameObject doorToDeactivate;
     public GameObject explosionEffect;
 
+    private AudioSource audioSource;
+    public AudioSource doorAudioSource;
+
+    private bool doorDeactivated = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); // Get the AudioSource component on this GameObject
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player1"))
         {
 
             Debug.Log("player1 stood on pressure plate");
-            if (doorToDeactivate != null)
+            if (doorToDeactivate != null && !doorDeactivated)
             {
-                Destroy(doorToDeactivate);
+                //Destroy(doorToDeactivate);
                 //Instantiate(explosionEffect, transform.position, Quaternion.identity);
 
                 Vector3 doorPosition = doorToDeactivate.transform.position;
 
-                if (explosionEffect != null)
-                {
-                    Instantiate(explosionEffect, doorToDeactivate.transform.position, Quaternion.identity);
-                }
+                StartCoroutine(DeactivateAndExplode());
+
+                PlayPressurePlateSound();
+
+                //if (explosionEffect != null)
+                //{
+                //    Instantiate(explosionEffect, doorToDeactivate.transform.position, Quaternion.identity);
+                //}
             }
         }
 
         if (collision.CompareTag("Player2"))
         {
             Debug.Log("player2 stood on pressure plate");
-            if (doorToDeactivate != null)
+            if (doorToDeactivate != null && !doorDeactivated)
             {
-                Destroy(doorToDeactivate);
+                //Destroy(doorToDeactivate);
                 //Instantiate(explosionEffect, transform.position, Quaternion.identity);
 
                 Vector3 doorPosition = doorToDeactivate.transform.position;
 
-                if (explosionEffect != null)
-                {
-                    Instantiate(explosionEffect, doorToDeactivate.transform.position, Quaternion.identity);
-                }
+                StartCoroutine(DeactivateAndExplode());
+
+                PlayPressurePlateSound();
+
+                //if (explosionEffect != null)
+                //{
+                //    Instantiate(explosionEffect, doorToDeactivate.transform.position, Quaternion.identity);
+                //}
             }
+        }
+    }
+
+    private void PlayPressurePlateSound()
+    {
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.Play();
+        }
+    }
+
+    private IEnumerator DeactivateAndExplode()
+    {
+        yield return new WaitForSeconds(1.0f); // Wait for 1 second
+
+        Destroy(doorToDeactivate);
+        PlayDoorSound();
+        doorDeactivated = true;
+
+        if (explosionEffect != null)
+        {
+            Instantiate(explosionEffect, doorToDeactivate.transform.position, Quaternion.identity);
+        }
+    }
+
+    private void PlayDoorSound()
+    {
+        if (doorAudioSource != null)
+        {
+            doorAudioSource.PlayOneShot(doorAudioSource.clip);
         }
     }
 }
--- Snowball Fight/Assets/Scripts/TwoPlayerPressure.cs
+++ Snowball Fight/Assets/Scripts/TwoPlayerPressure.cs
@@ -10,6 +10,16 @@ public class TwoPlayerPressure : MonoBehaviour
     private bool player1Inside;
     private bool player2Inside;
 
+    private AudioSource audioSource;
+    public AudioSource doorAudioSource;
+
+    private bool doorDeactivated = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); // Get the AudioSource component on this GameObject
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player1"))
@@ -38,17 +48,51 @@ public class TwoPlayerPressure : MonoBehaviour
 
     private void TryOpenDoor()
     {
-        if (player1Inside && player2Inside)
+        if (player1Inside && player2Inside && !doorDeactivated)
         {
-            Destroy(doorToDeactivate);
+            //Destroy(doorToDeactivate);
             //Instantiate(explosionEffect, transform.position, Quaternion.identity);
 
             Vector3 doorPosition = doorToDeactivate.transform.position;
 
-            if (explosionEffect != null)
-            {
-                Instantiate(explosionEffect, doorToDeactivate.transform.position, Quaternion.identity);
-            }
+            StartCoroutine(DeactivateAndExplode());
+
+            PlayPressurePlateSound();
+
+            // (explosionEffect != null)
+            //{
+            //    Instantiate(explosionEffect, doorToDeactivate.transform.position, Quaternion.identity);
+            //}
+        }
+    }
+
+    private void PlayPressurePlateSound()
+    {
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.Play();
+        }
+    }
+
+    private IEnumerator DeactivateAndExplode()
+    {
+        yield return new WaitForSeconds(1.0f); // Wait for 1 second
+
+        Destroy(doorToDeactivate);
+        PlayDoorSound();
+        doorDeactivated = true;
+
+        if (explosionEffect != null)
+        {
+            Instantiate(explosionEffect, doorToDeactivate.transform.position, Quaternion.identity);
+        }
+    }
+
+    private void PlayDoorSound()
+    {
+        if (doorAudioSource != null)
+        {
+            doorAudioSource.PlayOneShot(doorAudioSource.clip);
         }
     }
 }
